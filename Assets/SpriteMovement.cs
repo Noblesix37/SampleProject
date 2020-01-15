50ef3d09afc417a10d3e5e286b6a116d6a63488e
dillan <official.drb@gmail.com>
2020-01-15 12:21:45 -0700

    tried to update movemn ran into errors with class and file name

---
+++ Assets/SpriteMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    private float moveSpeed; 
+    private float rotateSpeed;
+
+    // Start is called before the first frame update
+    private class Pawn
+    {
+        private float movespeed = 10f;
+        private float rotatespeed = 90f;
+
+
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        transform.Translate(-Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        transform.Rotate(-Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Spritechnager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spritechanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer spriteRenderer;
+    
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+
+
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            Debug.LogError(message: "No Sprite Renderer Attached");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
