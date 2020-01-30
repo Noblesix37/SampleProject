9b83c3b9eb71e8dbf2aba34729399c6b9022e3b3
dillan <official.drb@gmail.com>
2020-01-30 12:26:33 -0700

    new movement

---
+++ Assets/SpriteMoving.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class SpriteMoving : MonoBehaviour
+{
+    private Transform tf;
+
+    public float rotationSpeed = 1.0f;
+
+    public float movementSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.right * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.position -= tf.up * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.position += tf.up * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position -= tf.right * movementSpeed * Time.deltaTime;
+        }
+    }
+}
